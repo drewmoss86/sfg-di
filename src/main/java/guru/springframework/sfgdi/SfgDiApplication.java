dba0bd892ae5506b198ee8e4805be4389c4b9e00
Andrew Moss <drew.moss86@gmail.com>
2020-07-15 21:49:26 -0700

    Spring Profiles

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +11,9 @@ public class SfgDiApplication {
     public static void main(String[] args) {
 
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+        I18NController i18N = (I18NController) ctx.getBean("i18NController");
+        System.out.println(i18N.sayHello());
 
         //Classname, but lowercase by default. Also, without casting a MyController object, bean will just return an object type
         MyController myC = (MyController) ctx.getBean("myController");
